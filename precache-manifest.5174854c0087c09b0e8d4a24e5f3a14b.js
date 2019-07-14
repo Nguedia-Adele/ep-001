27f1920c06dbaf6f5ec26159e0db442d594ce8e6
Nguedia Adele <nguediaadele@yahoo.fr>
2019-07-14 14:50:53 +0100

    Updates

---
+++ precache-manifest.5174854c0087c09b0e8d4a24e5f3a14b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0183a27846f9835d8650678f4f69fed6",
+    "url": "/ep-001/index.html"
+  },
+  {
+    "revision": "33a7666699cbf1119ea8",
+    "url": "/ep-001/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "3611be0478a70edba599",
+    "url": "/ep-001/static/css/main.ff234528.chunk.css"
+  },
+  {
+    "revision": "33a7666699cbf1119ea8",
+    "url": "/ep-001/static/js/2.8588695c.chunk.js"
+  },
+  {
+    "revision": "3611be0478a70edba599",
+    "url": "/ep-001/static/js/main.751e61a6.chunk.js"
+  },
+  {
+    "revision": "b99416d1904416864ecb",
+    "url": "/ep-001/static/js/runtime~main.6f257d48.js"
+  },
+  {
+    "revision": "17ed81ee17aa887648048f0725ca7798",
+    "url": "/ep-001/static/media/Logo.17ed81ee.png"
+  },
+  {
+    "revision": "e1618bf234729d42a4154c2f25749127",
+    "url": "/ep-001/static/media/baptism.e1618bf2.jpg"
+  },
+  {
+    "revision": "31801fc78ae706c3d355cb7c1980a2c4",
+    "url": "/ep-001/static/media/icon.31801fc7.png"
+  }
+]);
